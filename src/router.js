a5e08de369e6e0aeafa1b8467cfd41d34d62c6a4
Gaurav Panchal <grvpanchal@gmail.com>
2025-01-13 01:50:08 -0500

    additional routes

--- src/router.js
+++ src/router.js
@@ -11,5 +11,25 @@ export default new Router({
       name: 'about',
       component: () => import(/* webpackChunkName: "about" */'./views/About.vue'),
     },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "about" */'./views/About.vue'),
+    },
+    {
+      path: '/progress',
+      name: 'progress',
+      component: () => import(/* webpackChunkName: "about" */'./views/About.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'routine',
+      component: () => import(/* webpackChunkName: "about" */'./views/About.vue'),
+    },
+    {
+      path: '/goals',
+      name: 'goals',
+      component: () => import(/* webpackChunkName: "about" */'./views/About.vue'),
+    },
   ],
 });
